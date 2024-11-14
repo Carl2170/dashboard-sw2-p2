ada4dbd72419e14bce8797c90fee7714357642c5
CVargas2170 <cvargas2170@gmail.com>
2024-11-14 17:15:43 -0400

    requemientos actualizaos

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:Eyeoftiger123@localhost:5432/tabla_dashboard")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://postgres:Eyeoftiger123@localhost:5432/dashboard")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
